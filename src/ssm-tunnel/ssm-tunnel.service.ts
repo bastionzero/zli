f65d6a51b74919ae20a0b3f6a0f05c60f2f98c10
Sebby <lipmas93@gmail.com>
2021-03-09 18:45:53 -0500

    Refactor ssm-tunnel-service to use common ssm-tunnel-websocket-service code
    This abstacts most of the keysplitting and websocket logic around ssh
    tunneling into a common reusable service.

--- src/ssm-tunnel/ssm-tunnel.service.ts
+++ src/ssm-tunnel/ssm-tunnel.service.ts
@@ -10,19 +10,18 @@ import { HubConnection, HubConnectionBuilder, HubConnectionState } from '@micros
 import { Logger } from '../logger.service/logger';
 import { ConfigService } from '../config.service/config.service';
 import { KeySplittingService } from '../../webshell-common-ts/keysplitting.service/keysplitting.service';
-import { AddSshPubKeyMessage, HUB_RECEIVE_MAX_SIZE, SsmTunnelHubIncomingMessages, SsmTunnelHubOutgoingMessages, StartTunnelMessage, TunnelDataMessage, WebsocketResponse } from './ssm-tunnel.types';
-import { SynMessageWrapper, DataMessageWrapper, SynAckMessageWrapper, DataAckMessageWrapper, KeySplittingMessage, ErrorMessageWrapper, KeysplittingErrorTypes } from '../../webshell-common-ts/keysplitting.service/keysplitting-types';
-import { SignalRLogger } from '../../webshell-common-ts/logging/signalr-logger';
+
 import { SsmTargetService } from '../http.service/http.service';
 import { SsmTargetSummary } from '../http.service/http.service.types';
+import { SsmTunnelWebsocketService } from '../../webshell-common-ts/ssm-tunnel-websocket.service/ssm-tunnel-websocket.service';
+import { ZliAuthConfigService } from '../config.service/zli-auth-config.service';
+import { SsmTunnelTargetInfo } from '../../webshell-common-ts/ssm-tunnel-websocket.service/ssm-tunnel-websocket.types';
 
 export class SsmTunnelService
 {
-    private sequenceNumber = 0;
+    private ssmTunnelWebsocketService: SsmTunnelWebsocketService;
     private sendQueue: async.QueueObject<Buffer>;
-    private websocket : HubConnection;
     private errorSubject: Subject<string> = new Subject<string>();
-    private target: SsmTargetSummary;
     public errors: Observable<string> = this.errorSubject.asObservable();
 
     constructor(
@@ -33,7 +32,7 @@ export class SsmTunnelService
     {
         // https://caolan.github.io/async/v3/docs.html#queue
         this.sendQueue = async.queue(async (data: Buffer, cb) => {
-            await this.sendDataWorker(data);
+            await this.ssmTunnelWebsocketService.sendData(data);
             cb();
         });
     }
@@ -45,202 +44,33 @@ export class SsmTunnelService
         identityFile: string
     ) : Promise<boolean> {
         try {
-            this.target = await this.getSsmTargetFromHostString(hostName);
+            let target = await this.getSsmTargetFromHostString(hostName);
 
-            await Promise.all([
-                this.setupWebsocket(),
-                this.setupEphemeralSshKey(identityFile)
-            ]);
+            this.ssmTunnelWebsocketService = new SsmTunnelWebsocketService(
+                this.logger,
+                this.keySplittingService,
+                new ZliAuthConfigService(this.configService),
+                target as SsmTunnelTargetInfo
+            );
 
-            await this.sendStartTunnelMessage({
-                targetId: this.target.id,
-                targetPort: port,
-                targetUser: userName
-            });
+            // Forward errors from the SsmTunnelWebsocketService
+            this.ssmTunnelWebsocketService.errors.subscribe(err => this.errorSubject.next(err));
 
-            await this.sendPubKeyFromIdentityFile(identityFile);
+            await this.setupEphemeralSshKey(identityFile);
+            let pubKey = await this.extractPubKeyFromIdentityFile(identityFile);
 
-            await this.performKeysplittingHandshake();
+            await this.ssmTunnelWebsocketService.setupWebsocketTunnel(userName, port, pubKey);
 
             return true;
         } catch(err) {
-            this.handleError(`Failed to setup tunnel: ${err}`);
+            this.logger.error(err);
+            this.errorSubject.next(err);
             return false;
         }
     }
 
-    public async performKeysplittingHandshake() {
-        if(this.target.agentVersion === '') {
-            this.logger.warn(`Skipping keysplitting handshake because agent version is not set for target ${this.target.id}`);
-            return;
-        }
-
-        this.logger.debug(`Starting keysplitting handshake with ${this.target.id}`);
-        this.logger.debug(`Agent Version ${this.target.agentVersion}, Agent ID: ${this.target.agentId}`);
-
-        await this.sendOpenShellSynMessage();
-    }
-
-    public async sendOpenShellSynMessage() {
-        if(this.target.agentId === '') {
-            throw new Error(`Unknown agentId in sendOpenShellSynMessage for target ${this.target.id}`);
-        }
-
-        await this.sendSynMessage({
-            synPayload: {
-                signature: '',
-                payload: {
-                    type: 'SYN',
-                    action: 'ssh/open',
-                    nonce: crypto.randomBytes(32).toString('base64'),
-                    targetId: this.target.agentId,
-                    BZECert: await this.keySplittingService.getBZECert(this.configService.getAuth())
-                }
-            }
-        });
-    }
-
-    public async sendOpenShellDataMessage() {
-        if(this.target.agentId === '') {
-            throw new Error(`Unknown agentId in sendOpenShellDataMessage for target ${this.target.id}`);
-        }
-
-        await this.sendDataMessage({
-            dataPayload: {
-                signature: '',
-                payload: {
-                    type: 'DATA',
-                    action: 'ssh/open',
-                    hPointer: 'placeholder',
-                    targetId: this.target.agentId,
-                    BZECert: await this.keySplittingService.getBZECertHash(this.configService.getAuth()),
-                    payload: 'payload'
-                }
-            }
-        });
-    }
-
     public sendData(data: Buffer) {
         this.sendQueue.push(data);
-    }
-
-    private async sendDataWorker(data: Buffer) {
-        let base64EncData = data.toString('base64');
-        let len = base64EncData.length;
-
-        try {
-            // Batch the send data so that an individual message stays below the
-            // HUB_RECEIVE_MAX_SIZE limit
-            let offset = 0;
-
-            // Give some slack for the rest of the TunnelDataMessage (sequence
-            // number + json encoding)
-            let maxChunkSize = HUB_RECEIVE_MAX_SIZE - 1024;
-
-            while(offset < len) {
-
-                let chunkSize = Math.min(len - offset, maxChunkSize);
-                let dataMessage: TunnelDataMessage = {
-                    data: base64EncData.substr(offset, chunkSize),
-                    sequenceNumber: this.sequenceNumber++
-                };
-                offset += chunkSize;
-
-                await this.sendWebsocketMessage<TunnelDataMessage>(
-                    SsmTunnelHubOutgoingMessages.SendData,
-                    dataMessage
-                );
-            }
-        } catch(err) {
-            this.handleError(err);
-        }
-    }
-
-    public async closeConnection() {
-        if(this.websocket) {
-            await this.websocket.stop();
-            this.websocket = undefined;
-        }
-    }
-
-    private async setupWebsocket() {
-        await this.startWebsocket();
-
-        this.websocket.onclose((error) => {
-            this.handleError(`Websocket was closed by server: ${error}`);
-        });
-
-        // Set up ReceiveData handler
-        this.websocket.on(SsmTunnelHubIncomingMessages.ReceiveData, (dataMessage: TunnelDataMessage) => {
-            try {
-                let buf = Buffer.from(dataMessage.data, 'base64');
-
-                this.logger.debug(`received tunnel data message with sequence number ${dataMessage.sequenceNumber}`);
-
-                // Write to standard out for ProxyCommand to consume
-                process.stdout.write(buf);
-            } catch(e) {
-                this.logger.error(`Error in ReceiveData: ${e}`);
-            }
-        });
-
-        // Set up receive message handlers
-        this.websocket.on(SsmTunnelHubIncomingMessages.ReceiveSynAck, (synAckMessage: SynAckMessageWrapper) => {
-            try {
-                this.logger.debug(`Received SynAck message: ${JSON.stringify(synAckMessage)}`);
-
-                // Validate our HPointer
-                if (this.keySplittingService.validateHPointer(synAckMessage.synAckPayload.payload.hPointer) != true) {
-                    let errorString = '[SynAck] Error Validating HPointer!';
-                    this.logger.error(errorString);
-                    throw new Error(errorString);
-                }
-
-                this.sendOpenShellDataMessage();
-            } catch (e) {
-                this.logger.error(`Error in ReceiveSynAck: ${e}`);
-            }
-        });
-        this.websocket.on(SsmTunnelHubIncomingMessages.ReceiveDataAck, (dataAckMessage: DataAckMessageWrapper) => {
-            try {
-                this.logger.debug(`Received DataAck message: ${JSON.stringify(dataAckMessage)}`);
-
-                // Validate our HPointer
-                if (this.keySplittingService.validateHPointer(dataAckMessage.dataAckPayload.payload.hPointer) != true) {
-                    let errorString = '[DataAck] Error Validating HPointer!';
-                    this.logger.error(errorString);
-                    throw new Error(errorString);
-                }
-
-            } catch (e) {
-                this.logger.error(`Error in ReceiveDataAck: ${e}`);
-            }
-        });
-
-        this.websocket.on(SsmTunnelHubIncomingMessages.ReceiveError, (errorMessage: ErrorMessageWrapper) => {
-            let errorPayload = errorMessage.errorPayload.payload;
-
-            // TODO: check signature on error payload
-
-            this.logger.error(`
-                Got agent keysplitting error on message ${errorPayload.hPointer}
-                Error Type: ${errorPayload.errorType}
-                Error Message: ${errorPayload.message}
-            `);
-
-            switch(errorPayload.errorType) {
-                case KeysplittingErrorTypes.BZECertIDTokenValidationError:
-                    this.handleIdTokenValidationError();
-                    break;
-                default:
-                    this.handleError(`Unhandled keysplitting error: ${errorPayload.message}`);
-            }
-        });
-    }
-
-    private handleIdTokenValidationError() {
-        // TODO: refresh oauth token
-        // If that still fails then logout
     }
 
     private async setupEphemeralSshKey(identityFile: string): Promise<void> {
@@ -276,19 +106,6 @@ export class SsmTunnelService
         return privateKey;
     }
 
-    private async sendPubKeyFromIdentityFile(identityFile: string) {
-        let pubKey = await this.extractPubKeyFromIdentityFile(identityFile);
-
-        // key type and pubkey are space delimited in the resulting string
-        // https://github.com/joyent/node-sshpk/blob/4342c21c2e0d3860f5268fd6fd8af6bdeddcc6fc/lib/formats/ssh.js#L99
-        let [keyType, sshPubKey] = pubKey.toString('ssh').split(' ');
-
-        await this.sendAddSshPubKeyMessage({
-            keyType: keyType,
-            publicKey: sshPubKey
-        });
-    }
-
     private async extractPubKeyFromIdentityFile(identityFileName: string): Promise<SshPK.Key> {
         let identityFile = await this.readIdentityFile(identityFileName);
 
@@ -300,56 +117,6 @@ export class SsmTunnelService
 
     private async readIdentityFile(identityFileName: string): Promise<string> {
         return util.promisify(fs.readFile)(identityFileName, 'utf8');
-    }
-
-    private createConnection(): HubConnection {
-        // sessionId is for user authentication
-        const queryString = `?session_id=${this.configService.sessionId()}`;
-        const connectionUrl = `${this.configService.serviceUrl()}api/v1/hub/ssm-tunnel/${queryString}`;
-
-        const connectionBuilder = new HubConnectionBuilder();
-        connectionBuilder.withUrl(
-            connectionUrl,
-            { headers: { authorization: this.configService.getAuthHeader() } }
-        ).configureLogging(new SignalRLogger(this.logger));
-        return connectionBuilder.build();
-    }
-
-    private async startWebsocket()
-    {
-        this.websocket = this.createConnection();
-        await this.websocket.start();
-    }
-
-    private async sendStartTunnelMessage(startTunnelMessage: StartTunnelMessage) {
-        await this.sendWebsocketMessage<StartTunnelMessage>(
-            SsmTunnelHubOutgoingMessages.StartTunnel,
-            startTunnelMessage
-        );
-    }
-
-    private async sendAddSshPubKeyMessage(addSshPubKeyMessage: AddSshPubKeyMessage) {
-        await this.sendWebsocketMessage<AddSshPubKeyMessage>(
-            SsmTunnelHubOutgoingMessages.AddSshPubKey,
-            addSshPubKeyMessage
-        );
-    }
-
-    private async sendWebsocketMessage<T>(methodName: string, message: T) {
-        if(this.websocket === undefined || this.websocket.state == HubConnectionState.Disconnected)
-            throw new Error('Hub disconnected');
-
-        let response = await this.websocket.invoke<WebsocketResponse>(methodName, message);
-
-        // Handle Hub Error
-        if(response.error) {
-            throw new Error(response.errorMessage);
-        }
-    }
-
-    private async handleError(errorMessage: string) {
-        this.logger.error(errorMessage);
-        this.errorSubject.next(errorMessage);
     }
 
     private async getSsmTargetFromHostString(host: string): Promise<SsmTargetSummary> {
@@ -388,23 +155,5 @@ export class SsmTunnelService
 
             return matchedTarget[0];
         }
-    }
-
-    public async sendSynMessage(synMessage: SynMessageWrapper): Promise<void> {
-        this.logger.debug('Sending syn message...');
-        await this.keySplittingService.setExpectedHPointerSyn(synMessage.synPayload.payload);
-        await this.sendWebsocketMessage<SynMessageWrapper>(
-            SsmTunnelHubOutgoingMessages.SynMessage,
-            synMessage
-        );
-    }
-
-    public async sendDataMessage(dataMessage: DataMessageWrapper): Promise<void> {
-        this.logger.debug('Sending data message...');
-        await this.keySplittingService.setExpectedHPointerData(dataMessage.dataPayload.payload);
-        await this.sendWebsocketMessage<DataMessageWrapper>(
-            SsmTunnelHubOutgoingMessages.DataMessage,
-            dataMessage
-        );
     }
 }
