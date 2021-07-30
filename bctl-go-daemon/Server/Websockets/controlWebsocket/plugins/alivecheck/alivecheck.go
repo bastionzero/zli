81f8dff7998ab9f807faad9bb0231928cf8ae043
Sid Premkumar <sid.premkumar@gmail.com>
2021-07-30 10:17:17 -0400

    fix valid cluster roles

--- bctl-go-daemon/Server/Websockets/controlWebsocket/plugins/alivecheck/alivecheck.go
+++ bctl-go-daemon/Server/Websockets/controlWebsocket/plugins/alivecheck/alivecheck.go
@@ -28,17 +28,22 @@ func AliveCheck(message controlWebsocketTypes.AliveCheckToClusterFromBastionSign
 	}
 
 	// Then get all cluster roles
-	clusterRoles, err := clientset.RbacV1().ClusterRoles().List(context.TODO(), metav1.ListOptions{})
+	clusterRoleBindings, err := clientset.RbacV1().ClusterRoleBindings().List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		panic(err.Error())
 	}
 
 	aliveCheckToBastionFromClusterMessage.ClusterRoles = []string{}
-	for _, clusterRole := range clusterRoles.Items {
-		// We do not consider any system:...  or eks:..., basically any system: looking roles as valid. This can be overridden from Bastion
-		var systemRegexPatten = regexp.MustCompile(`[a-zA-Z0-9]*:[a-za-zA-Z0-9-]*`)
-		if !systemRegexPatten.MatchString(clusterRole.Name) {
-			aliveCheckToBastionFromClusterMessage.ClusterRoles = append(aliveCheckToBastionFromClusterMessage.ClusterRoles, clusterRole.Name)
+	for _, clusterRoleBinding := range clusterRoleBindings.Items {
+		// Now loop over the subjects if we can find any user subjects
+		for _, subject := range clusterRoleBinding.Subjects {
+			if subject.Kind == "User" {
+				// We do not consider any system:... or eks:..., basically any system: looking roles as valid. This can be overridden from Bastion
+				var systemRegexPatten = regexp.MustCompile(`[a-zA-Z0-9]*:[a-za-zA-Z0-9-]*`)
+				if !systemRegexPatten.MatchString(subject.Name) {
+					aliveCheckToBastionFromClusterMessage.ClusterRoles = append(aliveCheckToBastionFromClusterMessage.ClusterRoles, subject.Name)
+				}
+			}
 		}
 	}
 
