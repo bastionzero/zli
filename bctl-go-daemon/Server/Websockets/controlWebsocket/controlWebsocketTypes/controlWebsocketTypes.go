4366b35f5f26eabf0864949f500f6f93fb6f3cec
Sid Premkumar <sidpremkumar@sids-mbp.myfiosgateway.com>
2021-08-06 13:27:27 -0400

    add support for storing data in kube secrets

--- bctl-go-daemon/Server/Websockets/controlWebsocket/controlWebsocketTypes/controlWebsocketTypes.go
+++ bctl-go-daemon/Server/Websockets/controlWebsocket/controlWebsocketTypes/controlWebsocketTypes.go
@@ -4,10 +4,19 @@ import (
 	"encoding/json"
 	"log"
 	"sync"
+	"os"
+	"bytes"
+	"encoding/gob"
+	"context"
 
 	"bastionzero.com/bctl/v1/commonWebsocketClient"
 
 	"github.com/gorilla/websocket"
+	coreV1 "k8s.io/api/core/v1"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	coreV1Types "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 type ProvisionNewWebsocketSignalRMessage struct {
@@ -48,6 +57,10 @@ type ControlWebsocket struct {
 	SocketLock sync.Mutex // Ref: https://github.com/gorilla/websocket/issues/119#issuecomment-198710015
 }
 
+type SecretConfig struct {
+	Test string
+}
+
 func (client *ControlWebsocket) SendAliveCheckToBastionFromClusterMessage(aliveCheckToBastionFromClusterMessage AliveCheckToBastionFromClusterMessage) error {
 	// Lock our mutex and setup the unlock
 	client.SocketLock.Lock()
@@ -74,4 +87,105 @@ func (client *ControlWebsocket) SendAliveCheckToBastionFromClusterMessage(aliveC
 	}
 	// client.SignalRTypeNumber++
 	return nil
+}
+
+func (client *ControlWebsocket) NewAgent() bool {
+	// Helper function to determin if this is a fresh install of an agent
+	// This works by checking the secret value 
+	secret := client.GetSecret()
+
+	// Now check the value of the secret
+	if (bytes.Compare(secret.Data["secret"], []byte("coolbeans")) == 0) {
+		return true
+	}
+	return false
+}
+
+func (client *ControlWebsocket) GetParsedSecret() SecretConfig {
+	// First get the kube secret
+	secret := client.GetSecret()
+	
+	// Now parse the bytes and get the secret data
+	secretConfig := client.DecodeToSecretConfig(secret.Data["secret"])
+
+	return secretConfig
+}
+
+func (client *ControlWebsocket) SaveParsedSecret(secretConfig SecretConfig) {
+	// First get the kube secret
+	secret := client.GetSecret()
+
+	// Now encode the secretConfig
+	encodedSecretConfig := client.EncodeToBytes(secretConfig)
+
+	// Now update the kube secret object
+	secret.Data["secret"] = encodedSecretConfig
+
+	// Now update the secret
+	client.UpdateSecret(secret)
+}
+
+func (client *ControlWebsocket) GetSecret() *coreV1.Secret {
+	// Build our client
+	secretsClient := client.GetSecretClient()
+
+	// Get the secret 
+	secretName := "bctl-" + os.Getenv("CLUSTER_NAME") + "-secret"
+	secret, err := secretsClient.Get(context.Background(), secretName, metaV1.GetOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Return the secret
+	return secret
+}
+
+func (client *ControlWebsocket) UpdateSecret(secret *coreV1.Secret) {
+	// Build our client
+	secretsClient := client.GetSecretClient()
+
+	// Update the secret
+	_, err := secretsClient.Update(context.Background(), secret, metaV1.UpdateOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func (client *ControlWebsocket) GetSecretClient() coreV1Types.SecretInterface {
+	// Create our api object
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Create our secrets client
+	secretsClient := clientset.CoreV1().Secrets(os.Getenv("NAMESPACE"))
+
+	return secretsClient
+} 
+
+func (client *ControlWebsocket) EncodeToBytes(p interface{}) []byte {
+	// Ref: https://gist.github.com/SteveBate/042960baa7a4795c3565
+	buf := bytes.Buffer{}
+	enc := gob.NewEncoder(&buf)
+	err := enc.Encode(p)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return buf.Bytes()
+}
+
+func (client *ControlWebsocket) DecodeToSecretConfig(s []byte) SecretConfig {
+	// Ref: https://gist.github.com/SteveBate/042960baa7a4795c3565
+	p := SecretConfig{}
+	dec := gob.NewDecoder(bytes.NewReader(s))
+	err := dec.Decode(&p)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return p
 }
