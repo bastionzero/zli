106a14f030a44d352c7f27ce05792d4fa33817d6
Yuval Marcus <ymarcus93@gmail.com>
2021-09-20 12:02:22 -0400

    Throw error if parameter sshConfigFile is not readable/accessable

--- src/handlers/quickstart/quickstart.command-builder.ts
+++ src/handlers/quickstart/quickstart.command-builder.ts
@@ -1,7 +1,10 @@
 import yargs from 'yargs';
 import os from 'os';
+import fs from 'fs';
 
 export type quickstartArgs = { sshConfigFile: string }
+
+const defaultSshConfigFilePath = `${os.homedir()}/.ssh/config`;
 
 export function quickstartCmdBuilder(yargs: yargs.Argv<{}>): yargs.Argv<quickstartArgs> {
     return yargs
@@ -11,8 +14,24 @@ export function quickstartCmdBuilder(yargs: yargs.Argv<{}>): yargs.Argv<quicksta
                 type: 'string',
                 demandOption: false,
                 alias: 'c',
-                describe: 'Path to ssh config file',
-                default: `${os.homedir()}/.ssh/config`
+                describe: `Path to ssh config file [default: ${defaultSshConfigFilePath}]`,
+                default: undefined
             }
-        );
+        )
+        .check(function (argv) {
+            if (argv.sshConfigFile === undefined) {
+                // User did not pass in sshConfigFile parameter. Use default parameter
+                argv.sshConfigFile = defaultSshConfigFilePath;
+                if (! fs.existsSync(argv.sshConfigFile)) {
+                    throw new Error(`Cannot read/access file at default path: ${argv.sshConfigFile}\nUse \`zli quickstart --sshConfigFile <filePath>\` to read a different file`);
+                }
+            } else {
+                // User passed in sshConfigFile
+                if (! fs.existsSync(argv.sshConfigFile)) {
+                    throw new Error(`Cannot read/access file at path: ${argv.sshConfigFile}`);
+                }
+            }
+
+            return true;
+        });
 }
